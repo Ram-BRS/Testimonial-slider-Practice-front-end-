2775a89121e760f653dea27779150988e38bd323
Ram-BRS <146158090+Ram-BRS@users.noreply.github.com>
2024-03-06 13:19:16 +0530

    update files

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -19,15 +19,15 @@
         customers.push(customer);
     }
 
-    createCustomer(0, 'Ram', 'HTML Stands for hypertext markup language. With HTML you can create your own websites HTML describe the structure of web pages, HTML consists of series of elements HTML elemmets tell the browser how to display the content');
+    createCustomer(0, 'Tim Berners-Lee', 'HTML Stands for hypertext markup language. With HTML you can create your own websites. HTML describe the structure of web pages, HTML consists of series of elements HTML elemmets tell the browser how to display the content');
 
-    createCustomer(1, 'Sravan', 'CSS Stands for cascading style sheets.  We use to style HTML documents. CSS describes how HTML elements are to be displayed on the screen or other media. CSS saves a lot of work and it can control the layout of multiple web pages all at once.');
+    createCustomer(1, 'Imaran Khan', 'CSS Stands for cascading style sheets.  We use to style HTML documents. CSS describes how HTML elements are to be displayed on the screen or other media. CSS saves a lot of work and it can control the layout of multiple web pages all at once.');
 
-    createCustomer(2, 'Rajesh', 'Javascript is a language mainly used for adding interactivity and dynanic content to webpages. It runs in web browsers. enabling developers to create interact user interfaces and enhance user experience.');
+    createCustomer(2, 'Brendan Eich', 'Javascript is a language mainly used for adding interactivity and dynanic content to webpages. It runs in web browsers. enabling developers to create interact user interfaces and enhance user experience.');
 
-    createCustomer(3, 'Dhoni', 'Datastructres: Ways of organizing and storing data efficiently, like arrays, linked lists, and trees.  Algorithms: Step-by-step procedures for solving problems, performing tasks on data structures, e.g., sorting or searching..');
+    createCustomer(3, 'Dennis Ritchie', 'Datastructres: Ways of organizing and storing data efficiently, like arrays, linked lists, and trees.  Algorithms: Step-by-step procedures for solving problems, performing tasks on data structures, e.g., sorting or searching..');
 
-    createCustomer(4, 'Sai Pallavi', 'React.js is an open-source JavaScript library for building user interfaces. It allows developers to create reusable UI components and efficiently manage the state of web applications. React is widely used for building interactive and dynamic single-page applications.');
+    createCustomer(4, 'React team', 'React.js is an open-source JavaScript library for building user interfaces. It allows developers to create reusable UI components and efficiently manage the state of web applications. React is widely used for building interactive and dynamic single-page applications.');
 
     btn.forEach(function (button) {
         button.addEventListener('click', function (e) {
